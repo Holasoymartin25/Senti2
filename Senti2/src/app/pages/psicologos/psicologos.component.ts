3f12b85868e856def9b4f91d92d1b47dcb6c900d
Martín Bernal González <mbergon2502@cpifpalanturing.es>
2026-01-23 09:41:11 +0100

    actualizacion dia 23 del 1

--- Senti2/src/app/pages/psicologos/psicologos.component.ts
+++ Senti2/src/app/pages/psicologos/psicologos.component.ts
@@ -14,20 +14,20 @@ export class PsicologosComponent {
             name: 'Dra. Elena Morales',
             title: 'Experta en Terapia de Pareja y Relaciones',
             description: 'Especialista en terapia de pareja. Ayuda a las parejas a reconstruir la confianza y mejorar la comunicación, con un enfoque empático para resolver conflictos.',
-            image: 'assets/elena.jpg'
+            image: 'assets/psychologist-1.jpg'
         },
         {
             name: 'Dr. Alejandro Torres',
             title: 'Experto en Ansiedad, Estrés y Gestión Emocional',
             description: 'Especialista en el manejo de la ansiedad y el estrés. Proporciona herramientas prácticas para la gestión emocional, ayudando a los pacientes a encontrar el equilibrio y la calma en su día a día.',
-            image: 'assets/alejandro.jpg',
+            image: 'assets/psychologist-2.jpg',
             highlight: true
         },
         {
             name: 'Dra. Sofía Ramos',
             title: 'Experta en Autoestima, Desarrollo Personal y Superación',
             description: 'Enfocada en el desarrollo personal y la autoestima. Guía a los pacientes a través de procesos de superación, ayudándoles a reconectar con su confianza y a construir una autoimagen positiva.',
-            image: 'assets/sofia.jpg'
+            image: 'assets/psychologist-3.jpg'
         }
     ];
 }
