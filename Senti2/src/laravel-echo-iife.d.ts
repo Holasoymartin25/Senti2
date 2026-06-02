729524a34de40cdd81c5255f629a0200593eaa93
Martin <martinhuawei25@gmail.com>
2026-06-02 20:20:23 +0200

    declaracion de clase Echo en d.ts para permitir instanciacion en produccion

--- Senti2/src/laravel-echo-iife.d.ts
+++ Senti2/src/laravel-echo-iife.d.ts
@@ -1,4 +1,12 @@
 declare module 'laravel-echo/iife' {
-  import type Echo from 'laravel-echo';
-  export default Echo;
+  interface EchoChannel<T = unknown> {
+    listen(event: string, callback: (data: T) => void): EchoChannel<T>;
+  }
+
+  export default class Echo<T = unknown> {
+    constructor(options: Record<string, unknown>);
+    disconnect(): void;
+    private(channel: string): EchoChannel<T>;
+    leave(channel: string): void;
+  }
 }
